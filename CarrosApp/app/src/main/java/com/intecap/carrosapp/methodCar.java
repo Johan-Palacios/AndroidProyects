e796f786422a2b5c866bc53a30b2a2fd5dddd5ea
Johan Palacios <johanpalacios.rivera@gmail.com>
2022-03-05 17:14:21 -0600

    App

--- CarrosApp/app/src/main/java/com/intecap/carrosapp/methodCar.java
+++ CarrosApp/app/src/main/java/com/intecap/carrosapp/methodCar.java
@@ -9,6 +9,95 @@ public class methodCar {
     public void setId(int id) {
         this.id = id;
     }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getLinea() {
+        return linea;
+    }
+
+    public void setLinea(String linea) {
+        this.linea = linea;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getTransmision() {
+        return transmision;
+    }
+
+    public void setTransmision(String transmision) {
+        this.transmision = transmision;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getKm() {
+        return km;
+    }
+
+    public void setKm(String km) {
+        this.km = km;
+    }
+
+    public String getTraccion() {
+        return traccion;
+    }
+
+    public void setTraccion(String traccion) {
+        this.traccion = traccion;
+    }
+
+    public String getCombustible() {
+        return combustible;
+    }
+
+    public void setCombustible(String combustible) {
+        this.combustible = combustible;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(String precio) {
+        this.precio = precio;
+    }
+
+    public String getCantidad_puertas() {
+        return cantidad_puertas;
+    }
+
+    public void setCantidad_puertas(String cantidad_puertas) {
+        this.cantidad_puertas = cantidad_puertas;
+    }
+
     public String getFoto() {
         return foto;
     }
@@ -16,42 +105,18 @@ public class methodCar {
     public void setFoto(String foto) {
         this.foto = foto;
     }
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getDescripsion() {
-        return descripsion;
-    }
-
-    public void setDescripsion(String descripsion) {
-        this.descripsion = descripsion;
-    }
-
-    public String getCantidad() {
-        return cantidad;
-    }
-
-    public void setCantidad(String cantidad) {
-        this.cantidad = cantidad;
-    }
-
-    public String getHash() {
-        return hash;
-    }
-
-    public void setHash(String hash) {
-        this.hash = hash;
-    }
 
     private int id;
-    private String nombre;
-    private String descripsion;
-    private String cantidad;
-    private String hash;
+    private String marca;
+    private String linea;
+    private String tipo;
+    private String transmision;
+    private String modelo;
+    private String km;
+    private String traccion;
+    private String combustible;
+    private String color;
+    private String precio;
+    private String cantidad_puertas;
     private String foto;
 }
